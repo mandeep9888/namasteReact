e49db922ba7a47eed30846120ab4f5bb25b7bc4c
Your Name <shaan9888@gmail.com>
2023-01-13 16:56:05 +0530

    week 2 class 1

--- week1_ignite_app/App.js
+++ week1_ignite_app/App.js
@@ -1,14 +1,44 @@
-const heading = React.createElement('h1',{}, "Hello from react !!!");
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/**
+ * parcel supports( read documentation)
+ * ---------------------
+ * 
+ * HMR - hot module replaement (reloading)
+ * File watcher algo(c++)
+ * compression 
+ * dev and production builds
+ * poly fills (replace variable names)
+ * port management
+ * minify
+ * bundling
+ * cleaning our code
+ * Image optimization
+ * super fast build algorithm
+ * cache while development
+ * compatibility with older browsers
+ * support Https 
+ * consistant hashing algo
+ * zero configuration
+ * 
+ * ------
+ * npx parcel build
+ * npx parcel index.html(entry point)
+ * 
+ */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = React.createElement('h1',{}, "Hello from react !!!");
 root.render(heading);
 
 const heading1 = React.createElement('h1',{
   id: "target1"
-}, "Heading 1")
+}, "Hello from parcel");
 const heading2 = React.createElement('h1',{
   id: "target2"
-}, "Heading 2")
+}, "Heading 2");
 
 const container = ReactDOM.createRoot(document.getElementById('container'));
 container.render([heading1, heading2]);
